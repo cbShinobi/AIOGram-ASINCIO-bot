e7b754594314b7683fc6bd51e7b4ac2bee012656
cbshinobi <cpthey1@gmail.com>
2023-03-23 10:38:06 +0400

    Доработка запроса ввода времени

--- bot/routers/user/create_game.py
+++ bot/routers/user/create_game.py
@@ -8,7 +8,6 @@ from ...services.database.models import BotUser, Game, GameMember
 from ...utils.form import Form, FormField
 from . import router
 
-
 class CreateGameForm(Form):
     name: str = FormField(enter_message_text=bot.phrases.enter_game_name)
     description: str = FormField(enter_message_text=bot.phrases.enter_game_description)
@@ -17,28 +16,30 @@ class CreateGameForm(Form):
         enter_message_text=bot.phrases.enter_game_datetime
     )
 
-
-@CreateGameForm.submit()
-async def create_game_form_submit(form: CreateGameForm, bot_user: BotUser):
+@CreateGameForm.field('starts_at')
+async def starts_at_field(value: str, field: FormField, bot_user: BotUser) -> datetime.datetime:
     try:
-        if form.starts_at <= datetime.datetime.now():
-            # Введенное значение времени меньше или равно текущему времени.
-            # Запись не разрешена.
-            raise ValueError(bot.phrases.game_created_message_error_text)
-        game = await Game.create(created_by=bot_user, **form.__dict__)
-        await GameMember.create(game=game, bot_user=bot_user)
-        await bot.send_message(
-            bot_user.id,
-            bot.phrases.game_created_message_text,
-            reply_markup=markups.start_markup,
-        )
+        starts_at = datetime.datetime.fromisoformat(value)
+        if starts_at <= datetime.datetime.now():
+            raise ValueError()
+        return starts_at
     except ValueError:
-        # Invalid time input, ask the user to enter the time again
         await bot.send_message(
             bot_user.id,
             bot.phrases.enter_game_datetime,
             reply_markup=types.ReplyKeyboardRemove(),
         )
+        return Form.SKIP_FIELD
+
+@CreateGameForm.submit()
+async def create_game_form_submit(form: CreateGameForm, bot_user: BotUser):
+    game = await Game.create(created_by=bot_user, **form.__dict__)
+    await GameMember.create(game=game, bot_user=bot_user)
+    await bot.send_message(
+        bot_user.id,
+        bot.phrases.game_created_message_text,
+        reply_markup=markups.start_markup,
+    )
 
 @router.message(F.text == bot.phrases.create_game_button_text)
 async def create_game_handler(message: types.Message, state: FSMContext):
