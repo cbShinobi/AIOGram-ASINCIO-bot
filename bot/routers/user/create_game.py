130d162da121118d8e757b151ea2b22cd32d7658
cbshinobi <cpthey1@gmail.com>
2023-03-23 09:53:35 +0400

    Доработка запроса ввода времени

--- bot/routers/user/create_game.py
+++ bot/routers/user/create_game.py
@@ -21,22 +21,25 @@ class CreateGameForm(Form):
 
 @CreateGameForm.submit()
 async def create_game_form_submit(form: CreateGameForm, bot_user: BotUser):
-    if form.starts_at <= datetime.datetime.now():
-        # Введенное значение времени меньше или равно текущему времени.
-        # Запись не разрешена.
+     try:
+        if form.starts_at <= datetime.datetime.now():
+            # Введенное значение времени меньше или равно текущему времени.
+            # Запись не разрешена.
+            raise ValueError(game_created_message_error_text)
+            game = await Game.create(created_by=bot_user, **form.__dict__)
+        await GameMember.create(game=game, bot_user=bot_user)
         await bot.send_message(
             bot_user.id,
-            bot.phrases.game_created_message_error_text,
+            bot.phrases.game_created_message_text,
+            reply_markup=markups.start_markup,
         )
-        return
-
-    game = await Game.create(created_by=bot_user, **form.__dict__)
-    await GameMember.create(game=game, bot_user=bot_user)
+    except ValueError:
+    # Invalid time input, ask the user to enter the time again
     await bot.send_message(
         bot_user.id,
-        bot.phrases.game_created_message_text,
-        reply_markup=markups.start_markup,
-    )
+        bot.phrases.enter_game_datetime,
+        reply_markup=types.ReplyKeyboardRemove(),
+        )
 
 @router.message(F.text == bot.phrases.create_game_button_text)
 async def create_game_handler(message: types.Message, state: FSMContext):
