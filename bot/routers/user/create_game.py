144257e5b17d4238473e8c831c2538fa68982809
cbshinobi <cpthey1@gmail.com>
2023-03-23 10:46:21 +0400

    Доработка запроса ввода времени

--- bot/routers/user/create_game.py
+++ bot/routers/user/create_game.py
@@ -8,38 +8,44 @@ from ...services.database.models import BotUser, Game, GameMember
 from ...utils.form import Form, FormField
 from . import router
 
+
 class CreateGameForm(Form):
     name: str = FormField(enter_message_text=bot.phrases.enter_game_name)
     description: str = FormField(enter_message_text=bot.phrases.enter_game_description)
     # location: str = FormField(enter_message_text=bot.phrases.enter_game_location)
-    starts_at: datetime.datetime = FormField(
-        enter_message_text=bot.phrases.enter_game_datetime
-    )
+    _starts_at: str = FormField(enter_message_text=bot.phrases.enter_game_datetime)
 
-@CreateGameForm.field('starts_at')
-async def starts_at_field(value: str, field: FormField, bot_user: BotUser) -> datetime.datetime:
+    @property
+    def starts_at(self) -> datetime.datetime:
+        return datetime.datetime.strptime(self._starts_at, '%Y-%m-%d %H:%M:%S')
+
+    @starts_at.setter
+    def starts_at(self, value: datetime.datetime):
+        self._starts_at = value.strftime('%Y-%m-%d %H:%M:%S')
+
+
+@CreateGameForm.submit()
+async def create_game_form_submit(form: CreateGameForm, bot_user: BotUser):
     try:
-        starts_at = datetime.datetime.fromisoformat(value)
-        if starts_at <= datetime.datetime.now():
-            raise ValueError()
-        return starts_at
+        if form.starts_at <= datetime.datetime.now():
+            # Введенное значение времени меньше или равно текущему времени.
+            # Запись не разрешена.
+            raise ValueError(bot.phrases.game_created_message_error_text)
+        game = await Game.create(created_by=bot_user, **form.__dict__)
+        await GameMember.create(game=game, bot_user=bot_user)
+        await bot.send_message(
+            bot_user.id,
+            bot.phrases.game_created_message_text,
+            reply_markup=markups.start_markup,
+        )
     except ValueError:
+        # Invalid time input, ask the user to enter the time again
         await bot.send_message(
             bot_user.id,
             bot.phrases.enter_game_datetime,
             reply_markup=types.ReplyKeyboardRemove(),
         )
-        return Form.SKIP_FIELD
 
-@CreateGameForm.submit()
-async def create_game_form_submit(form: CreateGameForm, bot_user: BotUser):
-    game = await Game.create(created_by=bot_user, **form.__dict__)
-    await GameMember.create(game=game, bot_user=bot_user)
-    await bot.send_message(
-        bot_user.id,
-        bot.phrases.game_created_message_text,
-        reply_markup=markups.start_markup,
-    )
 
 @router.message(F.text == bot.phrases.create_game_button_text)
 async def create_game_handler(message: types.Message, state: FSMContext):
